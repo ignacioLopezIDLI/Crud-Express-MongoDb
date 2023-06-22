aed2ea0f0607b40994b913553548c1067c6b26dc
ignacioLopezIDLI <ignaciolopez2000Q@gmail.com>
2023-06-22 08:26:03 -0400

    Conexión a MongoDB Integracion Modelo de Datos

--- src/database.js
+++ src/database.js
@@ -1,1 +1,11 @@
-    
+import mongoose from "mongoose" 
+
+(async ()=>{
+    try {
+        const db = await mongoose.connect("mongodb://localhost:27017/crud-mongo")
+        console.log("Base Datos Conectada",db.connection.name)
+    } catch (error) {
+        console.log(error)
+    }
+})()
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from "./app"
+import "./database"
 
 app.listen(3000)
 console.log("Server en el puerto ",3000)
